b94ca06b1cfd00b14c9780e26349de56c62827f5
srujan <srujanprophet@gmail.com>
2018-11-13 11:19:46 +0530

    Add portfolio project

---
+++ portfolio-project/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Blog(models.Model):
+	title = models.CharField(max_length=40)
+	pub_date = models.DateTimeField(auto_now_add=True, blank=True)
+	body = models.TextField()
+	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return(self.title)
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
---
+++ portfolio-project/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Blog
+
+def allblogs(request):
+	posts = Blog.objects
+	return render(request,'blog/allblogs.html',{'posts':posts})
+
+def detail(request, blog_id):
+	detailblog = get_object_or_404(Blog, pk=blog_id)
+	return render(request,'blog/detail.html',{'detail':detailblog})
+
